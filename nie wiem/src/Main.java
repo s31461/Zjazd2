22ca2d06ff835c7519a46a425d76faf62fdce329
Łukasz Benedyczak <s31461@pjwstk.edu.pl>
2024-04-07 18:13:40 +0200

    nie wiem

--- nie wiem/src/Main.java
+++ nie wiem/src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
 
-        }
     }
 }
---
+++ nie wiem/src/Student.java
@@ -0,0 +1,16 @@
+public class Student {
+    Student student=new Student
+    public void fname() {
+    }
+    public void lname() {
+    }
+    public void indexNumber() {
+    }
+    public void email() {
+    }
+    public void address() {
+    }
+    public void grades() {
+    }
+}
+
